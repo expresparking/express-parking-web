e23341d3cf6e481d45525c38056a6524166430ea
expresparking <nebyatbn@gmail.com>
2026-09-07 13:23:14 -0400

    Load review fixes after site polish

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
 import "./polish.css";
+import "./review-fixes.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
